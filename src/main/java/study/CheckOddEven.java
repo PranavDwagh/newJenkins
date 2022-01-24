7594555d5c7db1eb787e2489f639970dcdb75c60
pranav wagh <waghpranav222@gmail.com>
2022-01-24 18:17:31 +0530

    first commit

---
+++ src/main/java/study/CheckOddEven.java
@@ -0,0 +1,14 @@
+package study;
+
+public class CheckOddEven {
+	public static void main(String[] args) {
+		
+		System.out.println("!!!....Result :"+checkOddEven(22)+"....!!!");
+		
+	}
+	public static boolean checkOddEven(int num)
+	{
+		return num%2 == 0;
+	}
+
+}
---
+++ src/test/java/study/Test.java
@@ -0,0 +1,13 @@
+package study;
+
+import static org.junit.Assert.assertTrue;
+
+public class Test {
+
+	@org.junit.Test
+	public void testInputIsEven()
+	{
+		assertTrue(CheckOddEven.checkOddEven(22));
+	}
+	
+}
